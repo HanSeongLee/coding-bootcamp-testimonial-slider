d92e5d3a3de27a18a893d79eca748c1b3d1acb30
HanSeongLee <jkhanseong@gmail.com>
2023-06-05 23:04:21 +0900

    ADD | Font

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google';
+
+const inter = Inter({
+  weight: ['300', '500', '700'],
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <style jsx global>{`
+          :root {
+            --inter-font: ${inter.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
