090d88515ab944e81582131c30b96b4a5fad95f5
HanSeongLee <jkhanseong@gmail.com>
2023-06-14 20:06:21 +0900

    ADD | SliderContainer

---
+++ containers/SliderContainer/index.tsx
@@ -0,0 +1,72 @@
+import React, { HTMLAttributes, ReactNode, useEffect, useState } from 'react';
+import Slider from 'components/Slider';
+import Slide from 'components/Slide';
+
+interface IProps extends HTMLAttributes<HTMLDivElement> {
+    children: ReactNode[];
+}
+
+const SliderContainer: React.FC<IProps> = ({ children }) => {
+    const [activeIndex, setActiveIndex] = useState<number>(0);
+
+    const onPrev = () => {
+        if (activeIndex === 0) {
+            return;
+        }
+        setActiveIndex(activeIndex - 1);
+    };
+
+    const onNext = () => {
+        if (activeIndex === children.length - 1) {
+            return;
+        }
+        setActiveIndex(activeIndex + 1);
+    };
+
+    const onKeyUp = (e: KeyboardEvent) => {
+        switch (e.key) {
+            case 'ArrowLeft':
+                onPrev();
+                break;
+            case 'ArrowRight':
+                onNext();
+                break;
+        }
+    };
+
+    const onWheel = (e: WheelEvent) => {
+        if (Math.abs(e.deltaX) < 50) {
+            return;
+        }
+        if (e.deltaX < 0) {
+            onPrev();
+        } else {
+            onNext();
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('wheel', onWheel);
+        window.addEventListener('keyup', onKeyUp);
+
+        return () => {
+            window.removeEventListener('wheel', onWheel);
+            window.removeEventListener('keyup', onKeyUp);
+        };
+    }, [activeIndex]);
+
+    return (
+        <Slider activeIndex={activeIndex}
+                onPrev={onPrev}
+                onNext={onNext}
+        >
+            {children.map((item, index) => (
+                <Slide key={index}>
+                    {item}
+                </Slide>
+            ))}
+        </Slider>
+    );
+};
+
+export default SliderContainer;
